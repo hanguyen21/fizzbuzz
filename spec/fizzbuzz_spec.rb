36522bddbe4fa2e94dffdbb7b20ea448198ff0ad
sarahc-dev <sarahclements@hotmail.co.uk>
2023-04-04 12:23:27 +0100

    return 'buzz' when multiple of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,4 +24,9 @@ describe 'fizzbuzz' do
     it 'returns "fizz" when passed 9' do
        expect(fizzbuzz(9)).to eq "fizz"
     end 
+
+    it 'returns "buzz" when passed a multiple of 5' do
+        expect(fizzbuzz(10)).to eq "buzz"
+        expect(fizzbuzz(20)).to eq "buzz"
+    end
     end
